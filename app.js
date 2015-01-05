f60f7c5b320cec10032443e769f61e8e982c0687
stormslowly <stormslowly@gmail.com>
2015-01-05 17:26:41 +0800

    express 3.x framework

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var robot = require('weixin-robot');
 var ruler = require('./lib/rules.js');
+var path = require('path');
 
 var app = express();
 
@@ -16,12 +17,28 @@ robot.watch(app, {
   path: '/robot'
 });
 
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.use(express.logger('dev'));
 app.use(express.cookieParser());
 app.use(express.session({
   secret: 'e4069e23bc38c7388810dfd60feec6ab',
   store: new express.session.MemoryStore()
 }));
+
+
 app.enable('trust proxy');
 app.listen(process.env.PORT || 3000, function() {
   console.log('robot on~');
