3a5a589022553770dbfeb270ca509381f970618b
stormslowly@gmail.com <stormslowly@gmail.com>
2015-05-17 08:35:29 +0800

    show text line info back

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,10 @@ var imagemin = require('gulp-imagemin');
 
 gulp.task('default', ['less']);
 
-gulp.task('less', function() {
+gulp.task('less', function () {
   return gulp.src('less/style.less')
     .pipe(less())
-    .on('error', function(e) {
+    .on('error', function (e) {
       gutil.log(e);
       this.emit('end');
     })
@@ -17,12 +17,12 @@ gulp.task('less', function() {
 });
 
 
-gulp.task('watch', function() {
-  gulp.watch('less/style.less', ['less']);
+gulp.task('watch', function () {
+  gulp.watch('less/*.less', ['less']);
 });
 
 
-gulp.task('imagemin', function() {
+gulp.task('imagemin', function () {
   return gulp.src('./public/images/ads/*.png')
     .pipe(imagemin({
       progressive: true,
