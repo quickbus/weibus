d753838db70e1033e04dde44c388c5f326e787d3
stormslowly@gmail.com <stormslowly@gmail.com>
2016-01-27 08:14:53 +0800

    update test case

--- test/testrules.js
+++ test/testrules.js
@@ -4,39 +4,39 @@ var robot = require('weixin-robot');
 var ruler = require('../lib/rules.js');
 var debug = require('debug')('weibus');
 
-describe('rules', function() {
+describe('rules', function () {
 
-  before(function() {
+  before(function () {
     ruler(robot);
   });
 
-  after(function() {
+  after(function () {
     robot.reset();
   });
 
-  it('should setup rules', function() {
+  it('should setup rules', function () {
     expect(robot.routes.length).above(2);
   });
 
-  it('answer to request', function(done) {
+  it('answer to request', function (done) {
 
     expect(robot.reply).to.be.ok;
 
-    robot.reply({}, function(err, info) {
+    robot.reply({}, function (err, info) {
       if (err) {
         done(err);
       }
-      expect(info.reply).to.contain('如:5');
+      expect(info.reply).to.equal('请先在 班车位置 按钮选择你要去的门店\n');
       done();
     });
   });
 
-  it('query route by nick name directly ', function(done) {
+  it('query route by nick name directly ', function (done) {
     robot.reply({
       text: '5号'
-    }, function(err, info) {
+    }, function (err, info) {
       try {
-        expect(info.reply).to.have.length(2);
+        expect(info.reply).to.equal('请先在 班车位置 按钮选择你要去的门店\n');
         debug('replies', info.reply);
         done();
       } catch (e) {
@@ -45,7 +45,7 @@ describe('rules', function() {
     });
   });
 
-  it('answer to subscribe', function(done) {
+  it('answer to subscribe', function (done) {
     var info = new robot.Info({
       type: 'event',
       param: {
@@ -54,7 +54,7 @@ describe('rules', function() {
 
     });
 
-    robot.reply(info, function(err, info) {
+    robot.reply(info, function (err, info) {
       if (err) {
         return done(err);
       }
@@ -64,12 +64,12 @@ describe('rules', function() {
   });
 
 
-  it.skip('answer to phone numbers', function(done) {
+  it.skip('answer to phone numbers', function (done) {
     var info = new robot.Info();
 
     info.text = '13524677703';
     info.session = {};
-    robot.reply(info, function(err, _info) {
+    robot.reply(info, function (err, _info) {
       if (err) {
         return done(err);
       }
@@ -79,7 +79,7 @@ describe('rules', function() {
         done(err);
       }
       info.text = '2';
-      robot.reply(info, function(err, _info) {
+      robot.reply(info, function (err, _info) {
         try {
           expect(_info.reply).to.have.length(2);
           done();
